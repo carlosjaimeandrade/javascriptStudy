666df2b4d0afdc28c5eac35b55e1a6f23f119432
Carlos jaime <jaime_andrek@hotmail.com>
2021-11-24 19:40:42 -0300

    MAP EXEMPLO

--- Array/aula 4 map.js
+++ Array/aula 4 map.js
@@ -1,11 +1,21 @@
-const nums = [1,2,3,4,5]
+const array = [{
+        nome: 'carlos'
+    },
+    {
+        nome: 'joão'
+    }
+]
 
 
 // for com proposito, gera um novo array não modiica o que recebeu
 
-let resultado = nums.map(function(e){
-        return e + "2"
+let result = array.map(function(e) {
+    let tr = `<td>${e.nome}</td>`
+    return tr
 })
 
-console.log(resultado)
+console.log(result)
 
+result.forEach(e => {
+    console.log(e)
+})
